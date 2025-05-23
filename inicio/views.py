0306a4bd4566c6f0d1cbf8d40b1a6ab46aef5a84
santiagopalma12 <spalmaa@unsa.edu.pe>
2025-05-22 23:54:22 -0500

    feat: sistema de templates creado y vista home renderizando home.html

--- inicio/views.py
+++ inicio/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("¡hola mundoo!")
+    return render(request, 'home.html')
+
 
 def another(request):
     return HttpResponse(f"Usuario: {request.user} | ¿Autenticado? {request.user.is_authenticated}")
